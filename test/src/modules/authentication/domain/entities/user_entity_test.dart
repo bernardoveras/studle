a91e883566f38be625f781a0f3c223bc9784708e
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-01 03:26:45 -0300

    test: refactor user entity test and add mock file

--- test/src/modules/authentication/domain/entities/user_entity_test.dart
+++ test/src/modules/authentication/domain/entities/user_entity_test.dart
@@ -3,15 +3,9 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:myschool/src/modules/authentication/domain/entities/user_entity.dart';
 
+import '../../../../../mocks/user_entity_mock.dart' as mock;
+
 void main() {
-  const defaultEntity = UserEntity(
-    id: 1,
-    accessToken: '6aa50a1a77db4163bcf3ee2a2a0115fb',
-    refreshToken: '48b38423c45f4daaab8d7ed782fb8dc8',
-    name: 'Bernardo Veras',
-    email: 'contato@bernardoveras.com.br',
-    cpf: '00000000000',
-  );
   test('UserEntity - equality', () {
     const entity1 = UserEntity(
       id: 1,
@@ -35,63 +29,45 @@ void main() {
   });
 
   test('UserEntity - to map', () {
-    const entity = defaultEntity;
+    const expectedEntity = mock.entity;
 
-    final map = entity.toMap();
+    final map = expectedEntity.toMap();
 
-    expect(map['id'], entity.id);
-    expect(map['access_token'], entity.accessToken);
-    expect(map['refresh_token'], entity.refreshToken);
-    expect(map['name'], entity.name);
-    expect(map['email'], entity.email);
-    expect(map['cpf'], entity.cpf);
+    expect(map['id'], expectedEntity.id);
+    expect(map['access_token'], expectedEntity.accessToken);
+    expect(map['refresh_token'], expectedEntity.refreshToken);
+    expect(map['name'], expectedEntity.name);
+    expect(map['email'], expectedEntity.email);
+    expect(map['cpf'], expectedEntity.cpf);
   });
 
   test('UserEntity - to json', () {
-    const entity = defaultEntity;
+    const expectedEntity = mock.entity;
 
-    final json = entity.toJson();
+    final json = expectedEntity.toJson();
 
     final map = jsonDecode(json);
 
-    expect(map['id'], entity.id);
-    expect(map['access_token'], entity.accessToken);
-    expect(map['refresh_token'], entity.refreshToken);
-    expect(map['name'], entity.name);
-    expect(map['email'], entity.email);
-    expect(map['cpf'], entity.cpf);
+    expect(map['id'], expectedEntity.id);
+    expect(map['access_token'], expectedEntity.accessToken);
+    expect(map['refresh_token'], expectedEntity.refreshToken);
+    expect(map['name'], expectedEntity.name);
+    expect(map['email'], expectedEntity.email);
+    expect(map['cpf'], expectedEntity.cpf);
   });
 
   test('UserEntity - from map', () {
-    const expectedEntity = defaultEntity;
+    const expectedEntity = mock.entity;
 
-    final map = <String, dynamic>{
-      'id': 1,
-      'access_token': '6aa50a1a77db4163bcf3ee2a2a0115fb',
-      'refresh_token': '48b38423c45f4daaab8d7ed782fb8dc8',
-      'name': 'Bernardo Veras',
-      'email': 'contato@bernardoveras.com.br',
-      'cpf': '00000000000',
-    };
-
-    final entity = UserEntity.fromMap(map);
+    final entity = UserEntity.fromMap(mock.map);
 
     expect(entity, expectedEntity);
   });
 
   test('UserEntity - from json', () {
-    const expectedEntity = defaultEntity;
+    const expectedEntity = mock.entity;
 
-    final json = jsonEncode({
-      'id': 1,
-      'access_token': '6aa50a1a77db4163bcf3ee2a2a0115fb',
-      'refresh_token': '48b38423c45f4daaab8d7ed782fb8dc8',
-      'name': 'Bernardo Veras',
-      'email': 'contato@bernardoveras.com.br',
-      'cpf': '00000000000',
-    });
-
-    final entity = UserEntity.fromJson(json.trim());
+    final entity = UserEntity.fromJson(mock.json);
 
     expect(entity, expectedEntity);
   });
