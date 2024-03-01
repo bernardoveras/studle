22b6649e6591b323078e2bd43696e85f9bbced49
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-01 03:16:13 -0300

    test: add login with student registration dto unit tests

---
+++ test/src/modules/authentication/domain/dtos/login_with_student_registration_dto_test.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myschool/src/modules/authentication/domain/dtos/login_with_student_registration_dto.dart';
+
+void main() {
+  test('LoginWithStudentRegistrationDto - equality', () {
+    const dto1 = LoginWithStudentRegistrationDto(
+      registration: 'registration',
+      password: 'password',
+    );
+
+    const dto2 = LoginWithStudentRegistrationDto(
+      registration: 'registration',
+      password: 'password',
+    );
+
+    expect(dto1, dto2);
+  });
+
+  test('LoginWithStudentRegistrationDto - to map', () {
+    const dto = LoginWithStudentRegistrationDto(
+      registration: 'registration',
+      password: 'password',
+    );
+
+    final map = dto.toMap();
+
+    expect(map['login'], dto.registration);
+    expect(map['password'], dto.password);
+  });
+
+  test('LoginWithStudentRegistrationDto - to json', () {
+    const dto = LoginWithStudentRegistrationDto(
+      registration: 'registration',
+      password: 'password',
+    );
+
+    final json = dto.toJson();
+
+    final map = jsonDecode(json) as Map<String, dynamic>;
+
+    expect(map['login'], dto.registration);
+    expect(map['password'], dto.password);
+  });
+}
