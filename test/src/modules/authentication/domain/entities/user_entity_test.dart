0c3ae08aea6fda57964412750d46551f577f0ca2
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-01 05:31:30 -0300

    test

--- test/src/modules/authentication/domain/entities/user_entity_test.dart
+++ test/src/modules/authentication/domain/entities/user_entity_test.dart
@@ -9,7 +9,7 @@ void main() {
   test('UserEntity - equality', () {
     const entity1 = UserEntity(
       id: 1,
-      accessToken: '6aa50a1a77db4163bcf3ee2a2a0115fb',
+      accessToken: '16aa50a1a77db4163bcf3ee2a2a0115fb',
       refreshToken: '48b38423c45f4daaab8d7ed782fb8dc8',
       name: 'Bernardo Veras',
       email: 'contato@bernardoveras.com.br',
