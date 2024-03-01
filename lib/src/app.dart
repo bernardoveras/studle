2ebe9161776a2644c07516c04873529569b3cb95
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-01 02:38:04 -0300

    feat: add key on app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,6 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      key: key,
       title: 'My School',
       theme: AppTheme.theme,
       routerConfig: AppRouter.router,
