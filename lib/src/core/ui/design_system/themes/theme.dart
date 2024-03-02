e3522a6344872f2e0958cbe47233e38a35c098c0
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-02 20:56:03 -0300

    style: add floating action button and tool tiptheme

--- lib/src/core/ui/design_system/themes/theme.dart
+++ lib/src/core/ui/design_system/themes/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../colors/colors.dart';
+import '../design_system.dart';
 
 abstract class AppTheme {
   static ThemeData theme = ThemeData(
@@ -9,6 +9,23 @@ abstract class AppTheme {
     bottomSheetTheme: const BottomSheetThemeData(
       backgroundColor: Colors.transparent,
       elevation: 0.0,
+    ),
+    tooltipTheme: TooltipThemeData(
+      decoration: BoxDecoration(
+        color: MonoChromaticColors.gray.v700.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      textStyle: const Button3Typography(
+        color: Colors.white,
+      ),
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: PrimaryColors.brand,
+      elevation: 0,
+      highlightElevation: 0,
+      focusElevation: 0,
+      hoverElevation: 0,
+      disabledElevation: 0,
     ),
     appBarTheme: AppBarTheme(
       backgroundColor: Colors.white,
