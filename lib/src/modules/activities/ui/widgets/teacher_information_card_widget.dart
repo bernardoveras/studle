f20a5ca9f31d9351b7604ed9bc71c09ff1a661ef
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-08 17:11:27 -0300

    feat: add new person information card generic

--- lib/src/modules/activities/ui/widgets/teacher_information_card_widget.dart
+++ lib/src/modules/activities/ui/widgets/teacher_information_card_widget.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
-import '../../../../core/ui/design_system/colors/colors.dart';
-import '../../../../core/ui/design_system/typographies/typographies.dart';
 import '../../../../core/ui/services/snackbar/snackbar_service.dart';
+import '../../../../core/ui/widgets/cards/person_information_card.dart';
 import '../../../../core/utils/copy_text.dart';
 
 class TeacherInformationCard extends StatelessWidget {
@@ -29,59 +28,11 @@ class TeacherInformationCard extends StatelessWidget {
           type: SnackBarType.success,
         );
       },
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: MonoChromaticColors.gray.v100,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox.square(
-              dimension: 48,
-              child: CircleAvatar(
-                backgroundColor: MonoChromaticColors.gray.v300,
-                backgroundImage:
-                    pictureUrl == null ? null : NetworkImage(pictureUrl!),
-                child: pictureUrl != null
-                    ? null
-                    : Text(
-                        name[0],
-                        style: Text1Typography(color: PrimaryColors.brand),
-                      ),
-              ),
-            ),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name,
-                    style: const Text2Typography(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    email,
-                    style: Text2Typography(
-                      fontWeight: FontWeight.w500,
-                      color: MonoChromaticColors.gray,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(width: 12),
-            Icon(
-              PhosphorIconsRegular.copy,
-              color: MonoChromaticColors.gray,
-            ),
-          ],
-        ),
+      child: PersonInformationCard(
+        name: name,
+        description: email,
+        pictureUrl: pictureUrl,
+        icon: PhosphorIconsRegular.copy,
       ),
     );
   }
