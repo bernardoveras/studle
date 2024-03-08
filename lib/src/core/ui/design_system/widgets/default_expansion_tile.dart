b806120c7b67b1ac082ab7609845b72887ad86e7
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-08 02:19:45 -0300

    refactor: remove change password when is unlogged, add mail redirect on question page items

--- lib/src/core/ui/design_system/widgets/default_expansion_tile.dart
+++ lib/src/core/ui/design_system/widgets/default_expansion_tile.dart
@@ -8,11 +8,17 @@ class DefaultExpansionTile extends StatelessWidget {
     required this.title,
     required this.description,
     this.showDivider = true,
+    this.textButton,
+    this.iconButton,
+    this.onPressedButton,
   });
 
   final String title;
   final String description;
   final bool showDivider;
+  final String? textButton;
+  final IconData? iconButton;
+  final VoidCallback? onPressedButton;
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +27,10 @@ class DefaultExpansionTile extends StatelessWidget {
       children: [
         ExpansionTile(
           shape: const Border(),
-          textColor: PrimaryColors.brand.v700,
+          iconColor: MonoChromaticColors.gray,
+          textColor: MonoChromaticColors.gray.v800,
           collapsedTextColor: MonoChromaticColors.gray.v800,
+          collapsedIconColor: MonoChromaticColors.gray,
           title: Text(
             title,
             style: const Text2Typography(
@@ -34,9 +42,26 @@ class DefaultExpansionTile extends StatelessWidget {
               width: double.infinity,
               child: Padding(
                 padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                child: Text(
-                  description,
-                  style: Text2Typography(color: MonoChromaticColors.gray),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      description,
+                      style: Text2Typography(
+                        color: MonoChromaticColors.gray,
+                      ),
+                    ),
+                    if (textButton != null) ...[
+                      const SizedBox(height: 24),
+                      SolidButton.primary(
+                        label: textButton!,
+                        icon: iconButton,
+                        onPressed: onPressedButton,
+                        style: SolidButtonStyle.outlined,
+                      ),
+                    ],
+                  ],
                 ),
               ),
             ),
