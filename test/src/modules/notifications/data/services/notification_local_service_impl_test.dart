c7505edeafaf1512aec6d39da2f7633ee33d65fc
bernardoveras <bernardo@vvssistemas.com.br>
2024-03-13 20:29:45 -0300

    test: add more unit test on notification service

--- test/src/modules/notifications/data/services/notification_local_service_impl_test.dart
+++ test/src/modules/notifications/data/services/notification_local_service_impl_test.dart
@@ -11,25 +11,6 @@ import 'package:studle/src/modules/notifications/domain/services/i_notification_
 import '../../../../../mocks/generated_mocks.mocks.dart';
 import '../../../../../mocks/notification_entity_mock.dart' as mock;
 
-/// 🕔 Unit Test Progress
-///
-/// ✅ [fetch] Success
-/// ✅ [fetch] Return empty list when notifications do not exist in local storage
-/// ✅ [fetch] Return error if the local storage read method returns error
-/// ✅ [fetch] Return error if local storage method returns a type other than List
-///
-/// ✅ [markAsRead] Success
-/// ✅ [markAsRead] Return error if the local storage read method returns error
-/// ✅ [markAsRead] Return error if the local storage write method returns error
-/// ✅ [markAsRead] Return error if local storage method returns a type other than List
-/// 🕔 [markAsRead] Return error if notification list does not contain id
-///
-/// 🕔 [markAsReadBatch] Success
-/// 🕔 [markAsReadBatch] Return error if the local storage read method returns error
-/// 🕔 [markAsReadBatch] Return error if the local storage write method returns error
-/// 🕔 [markAsReadBatch] Return error if local storage method returns a type other than List
-/// 🕔 [markAsReadBatch] Return error if notification list does not contain id
-
 void main() {
   late final ILocalStorageService localStorageService;
   late final INotificationService notificationService;
@@ -149,11 +130,122 @@ void main() {
       ));
     });
 
+    test('Return error if local storage method returns a type other than List',
+        () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => const Success('{"key":"value"}'));
+
+      final result = await notificationService.markAsRead(mock.entity.id);
+
+      expect(result.exceptionOrNull(), isA<InvalidTypeException>());
+      verifyNever(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      ));
+    });
+
     test('Return error if notification list does not contain id', () async {
       when(localStorageService.read<String>(LocalStorageKey.notifications))
           .thenAnswer((_) async => Success(mock.jsonUnreadList));
 
       final result = await notificationService.markAsRead(Guid.random());
+
+      expect(result.exceptionOrNull(), isA<NotFoundException>());
+      verifyNever(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      ));
+    });
+  });
+
+  group('markAsReadBatch', () {
+    test('Success', () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => Success(mock.jsonUnreadList));
+
+      when(localStorageService.write<String>(
+              LocalStorageKey.notifications, any))
+          .thenAnswer((_) async => const Success(true));
+
+      final result =
+          await notificationService.markAsReadBatch([mock.entity.id]);
+
+      expect(result.getOrNull(), true);
+      verify(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      )).called(1);
+    });
+
+    test('Return error if the local storage read method returns error',
+        () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => const Failure(UnknowException()));
+
+      final result =
+          await notificationService.markAsReadBatch([mock.entity.id]);
+
+      expect(result.exceptionOrNull(), isA<UnknowException>());
+      verifyNever(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      ));
+    });
+
+    test('Return error if the local storage write method returns error',
+        () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => Success(mock.jsonUnreadList));
+
+      when(localStorageService.write<String>(
+              LocalStorageKey.notifications, any))
+          .thenAnswer((_) async => const Failure(UnknowException()));
+
+      final result =
+          await notificationService.markAsReadBatch([mock.entity.id]);
+
+      expect(result.exceptionOrNull(), isA<UnknowException>());
+      verify(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      )).called(1);
+    });
+
+    test('Return error if local storage method returns a type other than List',
+        () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => const Success(''));
+
+      final result =
+          await notificationService.markAsReadBatch([mock.entity.id]);
+
+      expect(result.exceptionOrNull(), isA<UnknowException>());
+      verifyNever(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      ));
+    });
+
+    test('Return error if local storage method returns a type other than List',
+        () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => const Success('{"key":"value"}'));
+
+      final result =
+          await notificationService.markAsReadBatch([mock.entity.id]);
+
+      expect(result.exceptionOrNull(), isA<InvalidTypeException>());
+      verifyNever(localStorageService.write<String>(
+        LocalStorageKey.notifications,
+        any,
+      ));
+    });
+
+    test('Return error if notification list does not contain id', () async {
+      when(localStorageService.read<String>(LocalStorageKey.notifications))
+          .thenAnswer((_) async => Success(mock.jsonUnreadList));
+
+      final result = await notificationService.markAsReadBatch([Guid.random()]);
 
       expect(result.exceptionOrNull(), isA<NotFoundException>());
       verifyNever(localStorageService.write<String>(
